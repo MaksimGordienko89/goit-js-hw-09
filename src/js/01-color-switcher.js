e64bff2db37ef02074c679be465e14bced8e6d7c
MaksimGordienko89 <99180492+MaksimGordienko89@users.noreply.github.com>
2022-08-11 17:31:18 +0300

    task-01-edit

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,3 +1,24 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+startBtn = document.querySelector('button[data-start]');
+stopBtn = document.querySelector('button[data-stop]');
+body = document.querySelector('body');
+
+startBtn.addEventListener('click', changeColor);
+stopBtn.addEventListener('click', onBtnStop);
+
+let intervalId = null;
+
+function changeColor() {
+  //   const color = getRandomHexColor();
+  intervalId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.disabled = true;
+}
+
+function onBtnStop() {
+  clearInterval(intervalId);
+  startBtn.disabled = false;
+}
